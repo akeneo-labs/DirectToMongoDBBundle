905ba4753ac8d31368dbd7f1fb6a7490a8432d53
Benoit Jacquemont <benoit@akeneo.com>
2015-11-16 16:04:10 +0100

    Missing use on interface

--- Versioning/BulkVersionPersister.php
+++ Versioning/BulkVersionPersister.php
@@ -6,6 +6,7 @@ use Doctrine\ODM\MongoDB\DocumentManager;
 use Pim\Bundle\TransformBundle\Normalizer\MongoDB\VersionNormalizer;
 use Pim\Bundle\VersioningBundle\Builder\VersionBuilder;
 use Pim\Bundle\VersioningBundle\Doctrine\AbstractPendingMassPersister;
+use Pim\Bundle\VersioningBundle\Model\VersionableInterface;
 use Pim\Bundle\VersioningBundle\Manager\VersionContext;
 use Pim\Bundle\VersioningBundle\Manager\VersionManager;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
